d99a53ccb180edfeb147ec71e9057f401075b693
Jin0331 <sempre813@naver.com>
2023-12-27 12:26:51 +0900

    first app

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by JinwooLee on 12/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
