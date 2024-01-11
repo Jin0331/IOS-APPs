b31d5a3b778e1fdf5b13f9fe08debcdd198a33f7
Jin0331 <sempre813@naver.com>
2024-01-11 13:16:03 +0900

    중간저장

---
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by JinwooLee on 1/7/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var q : String
+    var a : String
+}
